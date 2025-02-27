a4f4b5eb1f14305891c3d5e242959ad411ec9965
bcwilliams376 <bcwilliams376@gmail.com>
2025-02-27 13:20:01 +0000

    kernels

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-import tmap as tm 
-import pandas as pd
+import tmap as tm
+import numpy as np
 from map4 import MAP4Calculator
 from rdkit import Chem
-import csv 
+import csv
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 def bring():
     smiles = []
@@ -12,9 +13,9 @@ def bring():
     file.close()
     return smiles
 
-def send(fingerprint_list):
-    wtr = csv.writer(open ('fingerprints.csv', 'w'), delimiter=';', lineterminator='\n')
-    for x in fingerprint_list : 
+def send(list, file):
+    wtr = csv.writer(open (file, 'w'), delimiter=';', lineterminator='\n')
+    for x in list : 
         wtr.writerow ([x])
     return "done"
 
@@ -34,4 +35,34 @@ for item in smiles_list:
     map4_a = MAP4.calculate(mol_a)
     fingerprint_list.append(map4_a)
 
-print(send(fingerprint_list))
+distances = []
+fingerprint_list_length = len(fingerprint_list)
+
+for x in range(fingerprint_list_length-1): 
+    map1 = fingerprint_list[x]
+    map2 = fingerprint_list[x+1]
+    distances.append(ENC.get_distance(map1, map2))
+
+print(distances)
+
+
+
+exp_kernel = []
+for distance in distances:
+    k = np.exp(-(distance)**2/1**2)
+    exp_kernel.append(k)
+
+print(send(exp_kernel, "Exponential_kernel.csv"))
+
+linear_kernel = []
+for distance in distances:
+    k = 1 - (distance)**2
+    linear_kernel.append(k)
+
+print(send(linear_kernel, "Linear_kernel.csv"))
+    
+
+
+   
+
+
